5c7ed36cf00d20e42b291c0df6b66f6dd80e88ab
Yujikawa <windbell0404@gmail.com>
2018-02-10 16:55:55 +0900

    Refactoring

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from PIL import Image, ImageDraw, ImageFont
 
 app = Flask(__name__)
 
+STATIC_IMAGE_PATH = "static/images"
+
 
 @app.route("/images/<string:category>/<string:width_height>", methods=['GET'])
-def maker(category, width_height):
+def maker_resize(category, width_height):
     """
     Make Sample Image
     :param category:
@@ -15,10 +17,19 @@ def maker(category, width_height):
     :return: Content-type=Image
     """
     (width, height) = width_height.split("x")
-    buf = make_image(category, width, height)
-    response = helpers.make_response(buf.getvalue())
-    response.headers["Content-type"] = "Image"
-    return response
+    buf = _make_resize_image(category, width, height)
+    return _make_response(buf)
+
+
+@app.route("/images/<string:category>", methods=['GET'])
+def maker_original(category):
+    """
+    Make Sample Image
+    :param category:
+    :return: Content-type=Image
+    """
+    buf = _make_original_image(category)
+    return _make_response(buf)
 
 
 @app.route("/images/categories", methods=['GET'])
@@ -27,11 +38,13 @@ def category():
     Get Image Categories
     :return: categories list
     """
-    result = os.listdir("static")
+    result = os.listdir(STATIC_IMAGE_PATH)
     return jsonify(categories=result)
 
 
-def make_image(category, width, height):
+# functions
+
+def _make_resize_image(category, width, height):
     """
     Make Sample Image(byte)
     :param category:
@@ -42,22 +55,72 @@ def make_image(category, width, height):
     width = int(width)
     height = int(height)
 
-    original_image_file_name = "{}.jpg".format(category)
-    original_image_file_path = os.path.join("static", category, original_image_file_name)
+    original_image_file_name, original_image_file_path = _get_original_file(category)
+
     if not os.path.isfile(original_image_file_path):
         return None
 
     img = Image.open(original_image_file_path)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("font/Lovelo Black.otf", int(width/10 + height/10))
-    draw.text((width/10, height/10), "SAMPLE", font=font, fill=(255, 255, 255, 100))
-    #img.thumbnail((int(width), int(height)))
+    # resize
     img = img.resize((width, height))
-    buf = BytesIO()
-    img.save(buf, 'png')
+    img, buf = _make_image_buffer(img)
     img.close()
 
     return buf
+
+
+def _make_original_image(category):
+    """
+    Make original image
+    :param category:
+    :return:
+    """
+
+    original_image_file_name, original_image_file_path = _get_original_file(category)
+
+    if not os.path.isfile(original_image_file_path):
+        return None
+    img = Image.open(original_image_file_path)
+    img, buf = _make_image_buffer(img)
+    img.close()
+
+    return buf
+
+
+def _make_image_buffer(img):
+    """
+    Make image buffer
+    :param img: PIL
+    :return: img, buf
+    """
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("font/Lovelo Black.otf", 30)
+    draw.text((0, 0), "SAMPLE", font=font, fill=(255, 255, 255, 100))
+    buf = BytesIO()
+    img.save(buf, 'jpeg')
+    return img, buf
+
+
+def _make_response(buf):
+    """
+    Make response
+    :param buf:
+    :return: response
+    """
+    response = helpers.make_response(buf.getvalue())
+    response.headers["Content-type"] = "Image"
+    return response
+
+
+def _get_original_file(category):
+    """
+    Get original file
+    :param category:
+    :return: original_image_file_name, original_image_file_path
+    """
+    original_image_file_name = "{}.jpg".format(category)
+    original_image_file_path = os.path.join(STATIC_IMAGE_PATH, category, original_image_file_name)
+    return original_image_file_name, original_image_file_path
 
 
 if __name__ == "__main__":
